f6a87db6f183331ccad037e1bc750f729dd45ee4
Jimmy Saavedra <jim312002@hotmail.com>
2025-05-03 01:44:04 -0500

    Validación para enviar datos a cultivo activo, falta enlazar el id del esp32

--- app/Http/Controllers/ControladorDatosSensor.php
+++ app/Http/Controllers/ControladorDatosSensor.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Models\Datos;
 use Illuminate\Http\Request;
 use Carbon\Carbon;
+use App\Models\Cultivo; 
 
 class ControladorDatosSensor extends Controller
 {
@@ -16,8 +17,16 @@ class ControladorDatosSensor extends Controller
             'ph' => 'required|numeric',
         ]);
 
+        // Buscar cultivo activo
+        $cultivoActivo = Cultivo::where('Estado', 'Activo')->latest()->first();
+
+        if (!$cultivoActivo) {
+            return response()->json(['error' => 'No hay cultivos activos'], 404);
+        }
+
         $data['hora'] = Carbon::now()->format('H:i:s');
-        $data['Id_cultivo'] = 1;
+        $data['Id_cultivo'] = $cultivoActivo->Id_cultivo;
+
         Datos::create($data);
 
         return response()->json(['status' => 'ok']);
